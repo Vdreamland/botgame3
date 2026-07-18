5a85a9b12960fb9d133910a72dfd1205d3cef220
Arashi <exterionsx@gmail.com>
2026-07-18 19:55:20 +0700

    Update

--- ai/memory.py
+++ ai/memory.py
@@ -1,41 +1,49 @@
-from typing import Set, List, Optional
-
 class BotMemory:
     def __init__(self):
-        self.failed_items: Set[str] = set()
-        self.failed_facilities: Set[str] = set()
-        self.failed_attacks: Set[str] = set()
-        self.move_history: List[str] = []
-        self.last_target_id: Optional[str] = None
-        self.last_action_type: Optional[str] = None
-        self.action_counter: int = 0
-        self.equipped_attempts: Set[str] = set()
-        self.drop_attempts: Set[str] = set()
-        self.use_attempts: Set[str] = set()
-        self.pickup_attempts: Set[str] = set()
-        self.death_regions: Set[str] = set()
+        self.failed_items = set()
+        self.failed_facilities = set()
+        self.failed_attacks = set()
+        self.move_history = []
+        self.last_target_id = None
+        self.last_action_type = None
+        self.action_counter = 0
+        self.equipped_attempts = set()
+        self.drop_attempts = set()
+        self.use_attempts = set()
+        self.pickup_attempts = set()
+        self.death_regions = set()
+        self.known_death_zones = set()
 
     def add_visited_region(self, region_id: str):
-        if region_id in self.move_history:
-            self.move_history.remove(region_id)
         self.move_history.append(region_id)
         if len(self.move_history) > 4:
             self.move_history.pop(0)
 
-    def track_action_failure(self, current_item_ids: Set[str], current_fac_ids: Set[str], current_enemy_ids: Set[str]):
-        if self.last_target_id:
-            target_exists = (self.last_target_id in current_item_ids) or (self.last_target_id in current_fac_ids) or (self.last_target_id in current_enemy_ids)
-            if target_exists:
+    def track_action_failure(self, current_item_ids, current_fac_ids, current_enemy_ids):
+        if self.last_action_type == "pickup" and self.last_target_id:
+            if self.last_target_id in current_item_ids:
                 self.action_counter += 1
-                if self.action_counter > 2:
-                    if self.last_action_type == "pickup":
-                        self.failed_items.add(self.last_target_id)
-                    elif self.last_action_type == "interact":
-                        self.failed_facilities.add(self.last_target_id)
-                    elif self.last_action_type == "attack":
-                        self.failed_attacks.add(self.last_target_id)
-                    self.last_target_id = None
-                    self.action_counter = 0
+                if self.action_counter >= 2:
+                    self.failed_items.add(self.last_target_id)
             else:
-                self.last_target_id = None
                 self.action_counter = 0
+                self.last_target_id = None
+                self.last_action_type = None
+        elif self.last_action_type == "interact" and self.last_target_id:
+            if self.last_target_id in current_fac_ids:
+                self.action_counter += 1
+                if self.action_counter >= 2:
+                    self.failed_facilities.add(self.last_target_id)
+            else:
+                self.action_counter = 0
+                self.last_target_id = None
+                self.last_action_type = None
+        elif self.last_action_type == "attack" and self.last_target_id:
+            if self.last_target_id in current_enemy_ids:
+                self.action_counter += 1
+                if self.action_counter >= 2:
+                    self.failed_attacks.add(self.last_target_id)
+            else:
+                self.action_counter = 0
+                self.last_target_id = None
+                self.last_action_type = None
